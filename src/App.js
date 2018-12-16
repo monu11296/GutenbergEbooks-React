eb4adb64d5528fa92d4019a1fb524323ed1e5a6c
Manoranjan Choudhary <monu11296@gmail.com>
2018-12-17 01:55:03 +0530

    Impovred fetching JSON data via Axios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import DisplayData from './DisplayData.js';
+
+
 import axios from 'axios';
-//import axios from 'axios-jsonp-pro';
+
+const API = 'http://gutendex.com/books.json/?mime_type=image';
+//const API = 'http://skunkworks.ignitesol.com:8000/books.json/?mime_type=image';
 
 
 class App extends Component {
@@ -14,32 +18,69 @@ class App extends Component {
     this.state = {
       results: [],
       error: null,
+      query: 'Fiction'
     };
+    
+    this.changeQuery = this.changeQuery.bind(this);
   }
 
-  
-  componentDidMount() {
+    changeQuery (newTopic){
+      this.setState({ query: newTopic })
+
+      axios.get(API, {
+        params: {topic: newTopic}
+      })
+        .then(res => this.setState({ results: res.data.results }))
+    }
 
 
-   // fetch('http://gutendex.com/books/?topic=fiction&mime_type=image')
-  //axios.get('http://skunkworks.ignitesol.com:8000/books/?topic=fiction&mime_type=image')
 
-  axios.get('http://gutendex.com/books.json/?mime_type=image')
-
-     //.then(data => this.setState({ results: data.results }))
-     .then(res => this.setState({ results: res.data.results }))
+  fetchData(){
+    axios.get(API, {
+      params: {topic: this.state.query}
+    })
+      //.then(data => this.setState({ results: data.results }))
+      .then(res => this.setState({ results: res.data.results }))
       .catch(error => this.setState({error: 'Hi'}));
-
   }
 
-  render() {
+  componentDidMount() {
+  //fetch('http://gutendex.com/books/?topic=fiction&mime_type=image')
+  /*
+            axios.get(API, {
+              params: {topic: this.state.query}
+            })
+              //.then(data => this.setState({ results: data.results }))
+              .then(res => this.setState({ results: res.data.results }))
+              .catch(error => this.setState({error: 'Hi'}));
+*/
+          this.fetchData();
+  }
+
+/*
+  componentDidUpdate(prevProps){
+    if( this.state.query !== prevProps.myQuery ){
+        //alert('Component is done rendering!');
+
+        axios.get(API, {
+          params: {topic: this.state.query}
+        }).then(res => this.setState({
+            results: res.data.results }))
+            
+    }
+   // return true;
+  }
+*/
+
+render() {
 
     return (
-        <ul>
-          <div className='DisplayData'>
-          <DisplayData  results={this.state.results}  />
-          </div>
-        </ul>
+
+      <ul>
+        <div className='DisplayData'>
+        <DisplayData  results={this.state.results}  onChange={this.changeQuery} myQuery={this.state.query}  />
+        </div>
+      </ul>
 
     );
   }
